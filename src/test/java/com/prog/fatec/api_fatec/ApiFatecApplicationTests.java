409f34c6a90e565c391e82a009281a7ec2b3c427
t2trizi <bea2bea17@gmail.com>
2025-08-13 19:27:57 -0300

    Adicionando a base do spring boot

---
+++ src/test/java/com/prog/fatec/api_fatec/ApiFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog.fatec.api_fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
